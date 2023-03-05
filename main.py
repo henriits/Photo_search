c965930c9866a17fbf3df93f57c1cc4d3a3de1ea
henriits <121551949+henriits@users.noreply.github.com>
2023-03-05 15:26:48 +0200

    reformate firstscreen class, with multiple methods

--- main.py
+++ main.py
@@ -2,13 +2,33 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 
+import wikipedia
+import requests
+
+
 Builder.load_file("frontend.kv")
 
 
 class FirstScreen(Screen):
-    def search_image(self):
-        self.manager.current_screen.ids.img.source = "images/normal.png"
+    def get_image_link(self):
+        # Get user query from TextInput
+        query = self.manager.current_screen.ids.user_query.text
+        # Get wikipedia page and the first image link
+        page = wikipedia.page(query)
+        image_link = page.images[0]
+        return image_link
 
+    def download_image(self):
+        # download the image
+        req = requests.get(self.get_image_link())
+        imagepath = "files/image.jpg"
+
+        with open(imagepath, "wb") as file:
+            file.write(req.content)
+        return imagepath
+    def set_image(self):
+        # Set the image in the image widget
+        self.manager.current_screen.ids.img.source = self.download_image()
 
 class RootWidget(ScreenManager):
     pass
